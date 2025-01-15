92d95359e31f107dab52cef3a6722714e79ad6ac
guitarbeat <83775388+guitarbeat@users.noreply.github.com>
2025-01-15 16:49:47 -0600

    idk

--- meow-namester-react/postcss.config.js
+++ meow-namester-react/postcss.config.js
@@ -1,35 +1,16 @@
 module.exports = {
   plugins: [
-    require('postcss-preset-env')({
-      stage: 0,
-      features: {
-        'custom-properties': false,
-        'nesting-rules': true,
-        'custom-media-queries': true
-      }
-    }),
-    require('postcss-combine-duplicated-selectors')({
-      removeDuplicatedProperties: true
-    }),
+    require('postcss-combine-duplicated-selectors')({ removeDuplicatedProperties: true }),
     require('postcss-merge-rules'),
-    require('postcss-sort-media-queries')({
-      sort: 'mobile-first'
-    }),
+    require('postcss-sort-media-queries'),
+    require('postcss-custom-properties')({ preserve: true }),
     require('autoprefixer'),
     require('cssnano')({
-      preset: ['default', {
-        cssDeclarationSorter: {
-          order: 'smacss'
-        },
+      preset: ['advanced', {
         discardComments: { removeAll: true },
-        discardDuplicates: true,
-        discardEmpty: true,
         mergeRules: true,
-        normalizeWhitespace: false,
-        minifySelectors: true,
-        minifyParams: true,
-        calc: true,
-        colormin: true
+        reduceIdents: false,
+        zindex: false
       }]
     })
   ]
